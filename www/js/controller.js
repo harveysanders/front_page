ce726a711af7b9e241b3f96eda8bd811ea7a62bb
Harvey Sanders <sanders.harvey@gmail.com>
2015-08-06 23:52:00 -0500

    define getFeedIndex function Please enter the commit message for your changes. Lines starting

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,11 +1,4 @@
-//(function( window ) {
-	var exampleResponseObject = {"responseData":{"feed":{"feedUrl":"http://www.npr.org/rss/rss.php?id=1007","title":"Science : NPR","link":"http://www.npr.org/templates/story/story.php?storyId=1007","author":"","description":"The latest health and science news. Updates on medicine, healthy living, nutrition, drugs, diet, and advances in science and technology. Subscribe to the Health & Science podcast.","type":"rss20","entries":[{"title":"It Took A Musician's Ear To Decode The Complex Song In Whale Calls","link":"http://www.npr.org/2015/08/06/427851306/it-took-a-musicians-ear-to-decode-the-complex-song-in-whale-calls?utm_medium=RSS&utm_campaign=science","author":"Bill McQuay","publishedDate":"Thu, 06 Aug 2015 02:00:00 -0700","contentSnippet":"Male humpback whales create \"songs\" together, scientists say. Katy Payne was the first to hear the shifts in pitch and pattern ...","content":"<p>Male humpback whales create \"songs\" together, scientists say. Katy Payne was the first to hear the shifts in pitch and pattern in the collective calls as complex music — haunting, evolving tunes.</p><p><a href=\"http://www.npr.org/templates/email/emailAFriend.php?storyId=427851306\">» E-Mail This</a></p>","categories":[]},{"title":"Why Did The U.S. Choose Hiroshima?","link":"http://www.npr.org/2015/08/06/429433621/why-did-the-u-s-choose-hiroshima?utm_medium=RSS&utm_campaign=science","author":"Geoff Brumfiel","publishedDate":"Thu, 06 Aug 2015 01:22:00 -0700","contentSnippet":"Seventy years ago, an atomic bomb wiped a city off the map. The committee that picked the target knew the destruction would be ...","content":"<p>Seventy years ago, an atomic bomb wiped a city off the map. The committee that picked the target knew the destruction would be awful, but hoped it could end the war and stop future use of such bombs.</p><p><a href=\"http://www.npr.org/templates/email/emailAFriend.php?storyId=429433621\">» E-Mail This</a></p>","categories":[]},{"title":"Green Pie In The Sky? Vertical Farming Is On The Rise In Newark","link":"http://www.npr.org/sections/thesalt/2015/08/05/429345848/green-pie-in-the-s…tical-farming-is-on-the-rise-in-newark?utm_medium=RSS&utm_campaign=science","author":"Joel Rose","publishedDate":"Wed, 05 Aug 2015 14:06:00 -0700","contentSnippet":"A former steel mill in New Jersey is getting a new lease on life as an indoor farm. AeroFarms is betting it can turn a profit ...","content":"<p>A former steel mill in New Jersey is getting a new lease on life as an indoor farm. AeroFarms is betting it can turn a profit growing greens with lights, using far less water than a traditional farm.</p><p><a href=\"http://www.npr.org/templates/email/emailAFriend.php?storyId=429345848\">» E-Mail This</a></p>","categories":[]},{"title":"Sharing Art Helps Medical Students Connect With Dementia Patients","link":"http://www.npr.org/sections/health-shots/2015/08/05/428153399/sharing-art-h…tudents-connect-with-dementia-patients?utm_medium=RSS&utm_campaign=science","author":"Ina Yang","publishedDate":"Wed, 05 Aug 2015 13:11:00 -0700","contentSnippet":"It's easy to spot what Alzheimer's patients have lost — memory, communication skills and more. But a study that sent medical ...","content":"<p>It's easy to spot what Alzheimer's patients have lost — memory, communication skills and more. But a study that sent medical students and dementia patients to art classes opened new vistas for all.</p><p><a href=\"http://www.npr.org/templates/email/emailAFriend.php?storyId=428153399\">» E-Mail This</a></p>","categories":[]},{"title":"Why You Shouldn't Take Your Milk's 3-Week Shelf Life For Granted","link":"http://www.npr.org/sections/thesalt/2015/08/05/429347700/why-you-shouldnt-t…ilks-three-week-shelf-life-for-granted?utm_medium=RSS&utm_campaign=science","author":"Nadia Whitehead","publishedDate":"Wed, 05 Aug 2015 10:28:00 -0700","contentSnippet":"What's the secret to long-lasting milk? A complex system of refrigeration called the cold chain. But in some parts of ...","content":"<p>What's the secret to long-lasting milk? A complex system of refrigeration called the cold chain. But in some parts of developing countries, the chain doesn't exist, leaving a lot of milk to spoil.</p><p><a href=\"http://www.npr.org/templates/email/emailAFriend.php?storyId=429347700\">» E-Mail This</a></p>","categories":[]},{"title":"Judge Strikes Down Idaho 'Ag-Gag' Law, Raising Questions For Other States","link":"http://www.npr.org/sections/thesalt/2015/08/04/429345939/idaho-strikes-down…law-raising-questions-for-other-states?utm_medium=RSS&utm_campaign=science","author":"Luke Runyon","publishedDate":"Tue, 04 Aug 2015 14:26:00 -0700","contentSnippet":"A judge ruled Monday that an Idaho law criminalizing undercover investigations of farms is unconstitutional. Seven other states ...","content":"<p>A judge ruled Monday that an Idaho law criminalizing undercover investigations of farms is unconstitutional. Seven other states have similar laws, but legal experts say they may not stand much longer.</p><p><a href=\"http://www.npr.org/templates/email/emailAFriend.php?storyId=429345939\">» E-Mail This</a></p>","categories":[]},{"title":"When Relying On The Sun, Energy Storage Remains Out Of Reach","link":"http://www.npr.org/2015/08/04/427734398/when-relying-on-the-sun-energy-storage-remains-out-of-reach?utm_medium=RSS&utm_campaign=science","author":"Leigh Paterson","publishedDate":"Tue, 04 Aug 2015 13:26:00 -0700","contentSnippet":"The ability to store energy could revolutionize the way electricity is made and used. But for many utility companies and ...","content":"<p>The ability to store energy could revolutionize the way electricity is made and used. But for many utility companies and regular folks, energy storage is still too costly and difficult.</p><p><a href=\"http://www.npr.org/templates/email/emailAFriend.php?storyId=427734398\">» E-Mail This</a></p>","categories":[]},{"title":"Straighten Your Hair Without Frying It? Engineers Are On The Case","link":"http://www.npr.org/sections/health-shots/2015/08/04/428982232/straighten-yo…ut-frying-it-engineers-are-on-the-case?utm_medium=RSS&utm_campaign=science","author":"Ina Yang","publishedDate":"Tue, 04 Aug 2015 11:00:00 -0700","contentSnippet":"Heat can turn curly locks into a sleek 'do — and can also damage hair permanently. Engineers at Purdue are figuring out how hot ...","content":"<p>Heat can turn curly locks into a sleek 'do — and can also damage hair permanently. Engineers at Purdue are figuring out how hot is just hot enough when it comes to wielding that flat iron.</p><p><a href=\"http://www.npr.org/templates/email/emailAFriend.php?storyId=428982232\">» E-Mail This</a></p>","categories":[]},{"title":"Women, There's A Reason Why You're Shivering In The Office","link":"http://www.npr.org/sections/health-shots/2015/08/04/429005094/women-theres-…ason-why-youre-shivering-in-the-office?utm_medium=RSS&utm_campaign=science","author":"Rae Ellen Bichell","publishedDate":"Tue, 04 Aug 2015 09:13:00 -0700","contentSnippet":"Air conditioning standards are based on the needs of a 155-pound man. Researchers say it's time to admit that women don't need ...","content":"<p>Air conditioning standards are based on the needs of a 155-pound man. Researchers say it's time to admit that women don't need to be chilled as much and crank up the thermostat.</p><p><a href=\"http://www.npr.org/templates/email/emailAFriend.php?storyId=429005094\">» E-Mail This</a></p>","categories":[]},{"title":"In The Fight Against Tsetse Flies, Blue Is The New Black","link":"http://www.npr.org/sections/goatsandsoda/2015/08/04/427181595/in-the-fight-…nst-tsetse-flies-blue-is-the-new-black?utm_medium=RSS&utm_campaign=science","author":"Maanvi Singh","publishedDate":"Tue, 04 Aug 2015 07:14:00 -0700","contentSnippet":"For some unknown reason, the insects that transmit sleeping sickness in sub-Saharan Africa are attracted to the color blue. So ...","content":"<p>For some unknown reason, the insects that transmit sleeping sickness in sub-Saharan Africa are attracted to the color blue. So scientists think blue flytraps could help wipe out the disease for good.</p><p><a href=\"http://www.npr.org/templates/email/emailAFriend.php?storyId=427181595\">» E-Mail This</a></p>","categories":[]}]}},"responseDetails":null,"responseStatus":200};
-
-
-
-	var data
-		;
-
+(function( window ) {
 
 	// example usage
 	// getRSSFeed('http://www.npr.org/rss/rss.php?id=1007', function(result){
@@ -40,78 +33,89 @@
 		console.log(x);
 	}
 
+	function stringer(e) {
+		console.log(JSON.stringify(e));
+	}
 	/**
- *	Part 1: put the RSS feed data into our format
- *  This function would take in the data as it comes from the RSS API and return something like
- * 	{
- *		title: "Feed title",
- * 		url: "http://blogurl.com",
- * 		feed_url: "http://blogurl.com/rss",
- * 		...
- * 		items: [
- * 			{	title: "article one", url: "http://blogurl.com/1", ... },
- * 			...
- * 		]
- *	}
+	 *	Part 1: put the RSS feed data into our format
+	 *  This function would take in the data as it comes from the RSS API and return something like
+	 * 	{
+	 *		title: "Feed title",
+	 * 		url: "http://blogurl.com",
+	 * 		feed_url: "http://blogurl.com/rss",
+	 * 		...
+	 * 		items: [
+	 * 			{	title: "article one", url: "http://blogurl.com/1", ... },
+	 * 			...
+	 * 		]
+	 *	}
 
 
- * 
- * The proper place to call this is probably INSIDE getRSSFeed before calling the callback,
- * since this method needs to know which properties the RSS feed supplies. Then everything outside
- * of getRSSFeed (and this helper) can be oblivious of all details about the source of the data.
- */
+	 * 
+	 * The proper place to call this is probably INSIDE getRSSFeed before calling the callback,
+	 * since this method needs to know which properties the RSS feed supplies. Then everything outside
+	 * of getRSSFeed (and this helper) can be oblivious of all details about the source of the data.
+	 */
 
- function extractRSSFeedInfo (rawFeedObject) {
- 	var RSSFeed = rawFeedObject.responseData.feed;
-	var currTime = Date.now();
-	var feedItems = RSSFeed.entries;
+	 function extractRSSFeedInfo (rawFeedObject) {
+	 	var RSSFeed = rawFeedObject.responseData.feed;
+		var currTime = Date.now();
+		var feedItems = RSSFeed.entries;
 
-	data = {
-		title: RSSFeed.title,
-		description: RSSFeed.description,
-		feed_url: RSSFeed.feedUrl,
-		url: RSSFeed.link,
-		updated_at: currTime,
-		items: []
-	};
+		data = {
+			title: RSSFeed.title,
+			description: RSSFeed.description,
+			feed_url: RSSFeed.feedUrl,
+			url: RSSFeed.link,
+			updated_at: currTime,
+			items: []
+		};
 
-	for (var i=0; i<feedItems.length; i++) {
-		var item = {};
-		item.title = feedItems[i].title;
-		item.contentSnippet = feedItems[i].contentSnippet;
-		item.url = feedItems[i].link;
-		item.published_at = feedItems[i].publishedDate;
-		item.is_read = false;
-		data.items.push(item); 
-	}return data;
- }
- 
-/**
- * Part 2: find out if our current data already contains this feed, and if so at which index in data.feeds
- * We're probably just looping through and looking at the feed_url
- */
- function getFeedIndex (data, feed) {
+		for (var i=0; i<feedItems.length; i++) {
+			var item = {};
+			item.title = feedItems[i].title;
+			item.contentSnippet = feedItems[i].contentSnippet;
+			item.url = feedItems[i].link;
+			item.published_at = feedItems[i].publishedDate;
+			item.is_read = false;
+			data.items.push(item); 
+		}return data;
+	 }
+	 
+	/**
+	 * Part 2: find out if our current data already contains this feed, and if so at which index in data.feeds
+	 * We're probably just looping through and looking at the feed_url
+	 */
+	 function getFeedIndex (data, feed) {
+	 	var key = feed.feed_url;
+	 	var feeds = data.feeds;
+	 	for (var i=0; i<feeds.length; i++) {
+	 		if (feeds[i].feed_url === key) {
+	 			return i;
+	 		}
+	 	} return -1;
+	 }
+	  
+	/**
+	 * Part 3a: If Part 2 decides we DON'T already have this feed,
+	 * we can probably just insert it at the end. This may not need to be a function of its own,
+	 * but sometimes reading a well-named function call (not that I've chosen the best names)
+	 * makes it way easier to follow by letting your brain stay in the higher-level logic. 
+	 */
+	 function insertNewFeed(data, feed) { data.feeds.push(feed); }
 
- }
-  
-/**
- * Part 3a: If Part 2 decides we DON'T already have this feed,
- * we can probably just insert it at the end. This may not need to be a function of its own,
- * but sometimes reading a well-named function call (not that I've chosen the best names)
- * makes it way easier to follow by letting your brain stay in the higher-level logic. 
- */
- function insertNewFeed(data, feed) { data.feeds.push(feed); }
+	/**
+	 * Part 3b: If part 2 decides we DO already have this feed,
+	 * we need to update data.feeds[feedIndex] with the new data in feed
+	 * 
+	 * For the most part, the new info in feed is going to supercede anything
+	 * we have stored. The exceptions would be any additional info we added,
+	 * that's not provided by the RSS feed; so far I think the is_read bit is it.
+	 * So... we could just update the is_read bits of feed with whatever's in
+	 * data.feeds[feedIndex] and then replace data.feeds[feedIndex] with feed
+	 */
+	 function updateFeed (data, feedIndex, feed) {
 
-/**
- * Part 3b: If part 2 decides we DO already have this feed,
- * we need to update data.feeds[feedIndex] with the new data in feed
- * 
- * For the most part, the new info in feed is going to supercede anything
- * we have stored. The exceptions would be any additional info we added,
- * that's not provided by the RSS feed; so far I think the is_read bit is it.
- * So... we could just update the is_read bits of feed with whatever's in
- * data.feeds[feedIndex] and then replace data.feeds[feedIndex] with feed
- */
- function updateFeed (data, feedIndex, feed) { }
+	 }
 
-//}(window));
+}(window));
